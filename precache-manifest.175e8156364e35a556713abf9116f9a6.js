73de3c2b53e6f15c6d51b0f8eebeb644eaaf745d
jjohnsundar <jjohnsundar9943@gmail.com>
2020-06-02 07:08:44 +0530

    Updates

---
+++ precache-manifest.175e8156364e35a556713abf9116f9a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "935853cbefbbef8ca3fe65b03385c168",
+    "url": "/Arrangegame/index.html"
+  },
+  {
+    "revision": "e3b19fe54fe9557733d0",
+    "url": "/Arrangegame/static/css/main.e4b60009.chunk.css"
+  },
+  {
+    "revision": "d2b4fb63d266298be07b",
+    "url": "/Arrangegame/static/js/2.4ad9e2c4.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Arrangegame/static/js/2.4ad9e2c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3b19fe54fe9557733d0",
+    "url": "/Arrangegame/static/js/main.d07edf5b.chunk.js"
+  },
+  {
+    "revision": "92aae1e988dbd329a40f",
+    "url": "/Arrangegame/static/js/runtime-main.44cb0024.js"
+  }
+]);
